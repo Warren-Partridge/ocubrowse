58e611f9d7eb85348eb434ef7570b9bafd81a1d4
Warren-Partridge <maps@perdix.org>
2018-12-01 16:28:29 -0500

    Add popup with button to trigger webgazer

--- js/background.js
+++ js/background.js
@@ -1,14 +1,16 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    //code: 'console.log("Clicked popup");'
-    //code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
-    file: "demo/public/javascripts/webgazer.js"
-  });
-  chrome.tabs.executeScript({
-    //code: 'console.log("Clicked popup");'
-    //code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
-    file: "js/start.js"
-  });
-});
 
+//
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.tabs.executeScript({
+//     //code: 'console.log("Clicked popup");'
+//     //code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
+//     file: "demo/public/javascripts/webgazer.js"
+//   });
+//   chrome.tabs.executeScript({
+//     //code: 'console.log("Clicked popup");'
+//     //code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
+//     file: "js/start.js"
+//   });
+// });
+//
 
---
+++ js/popup.js
@@ -0,0 +1,10 @@
+function startWebgazer() {
+  chrome.tabs.executeScript({
+    file: 'demo/public/javascripts/webgazer.js'
+  });
+  chrome.tabs.executeScript({
+    file: 'js/start.js'
+  });
+}
+
+document.getElementById('start-webgazer').addEventListener('click', startWebgazer);
