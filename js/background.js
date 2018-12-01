2d737fcc4bb53fdab4035384ee204f3d326bd606
Warren-Partridge <maps@perdix.org>
2018-12-01 00:41:21 -0500

    Put div appending in separate js file

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     //code: 'console.log("Clicked popup");'
-    code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
-    //file: "start.js"
+    //code: 'var div=document.createElement("div"); document.body.appendChild(div); div.innerText="test123";'
+    file: "js/start.js"
   });
 });
 
