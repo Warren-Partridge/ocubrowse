0b35e3b9080f23397349aac6e127164715fa7415
mhendrick98 <mhendric@bu.edu>
2018-12-01 17:45:41 -0500

    restructure

---
+++ chromeExtension/js/popup.js
@@ -0,0 +1,12 @@
+function startWebgazer() {
+  chrome.tabs.executeScript({
+    file: "js/webgazer.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/start.js"
+  });
+}
+
+document
+  .getElementById("start-webgazer")
+  .addEventListener("click", startWebgazer);
