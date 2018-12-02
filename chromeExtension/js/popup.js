d8474159e605eb403fffed25e9494d24efe80136
Keshav Maheshwari <keshavm02@gmail.com>
2018-12-02 06:35:34 -0500

    holy fuck dude

--- chromeExtension/js/popup.js
+++ chromeExtension/js/popup.js
@@ -1,24 +1,24 @@
-// function startWebgazer() {
-//   chrome.tabs.executeScript({
-//     file: "js/jquery.min.js"
-//   });
-//   chrome.tabs.executeScript({
-//     file: "js/webgazer.js"
-//   });
-//   chrome.tabs.executeScript({
-//     file: "js/makeBackButtonOverlay.js"
-//   });
-//   chrome.tabs.executeScript({
-//     file: "js/makeClickButtonOverlay.js"
-//   });
-//   chrome.tabs.executeScript({
-//     file: "js/start.js"
-//   });
-// }
-//
-// document
-//   .getElementById("start-webgazer")
-//   .addEventListener("click", startWebgazer);
+function startWebgazer() {
+  chrome.tabs.executeScript({
+    file: "js/jquery.min.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/webgazer.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/makeBackButtonOverlay.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/makeClickButtonOverlay.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/start.js"
+  });
+}
+
+document
+  .getElementById("start-webgazer")
+  .addEventListener("click", startWebgazer);
 
 
 
